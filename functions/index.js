ea7f685b5914fa5e093f43d24c2a412cbb3a23dd
Savath Khem <savath.khem@live.com>
2019-09-16 16:30:37 -0400

    Validation & Login Route

--- functions/index.js
+++ functions/index.js
@@ -69,13 +69,40 @@ app.post('/scream', (req, res) => {
         })
 });
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if(string.trim() === "") return true;
+    else return false;
+}
+
 // SignUp Route
 app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email, 
         password: req.body.password,
-        handle: req.body.handle,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
     };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if (newUser.password != newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
     // Validate
     let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
@@ -113,20 +140,36 @@ app.post('/signup', (req, res) => {
                 return res.status(500).json({ error: err.code});
             }
         });
-    // firebase
-    // .auth()
-    // .createUserWithEmailAndPassword(newUser.email, newUser.password)
-    // .then((data) => {
-    //     return res
-    //     .status(201)
-    //     .json({ message: `user ${data.user.uid} signed up succesfully`});
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    //     return res.status(500).json({ error: err.code });
-    // });
+});  
+
+app.post('/login', (req, res) => {
+
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = 'Must not be empty';
+    if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/wrong-password'){
+                return res.status(403).json({ general: 'Wrong credientials, please try again'})
+            } else return res.status(500).json({ error: err.code });
+        });
 });
-    
 
 // htpps://baseurl.com/api
 exports.api = functions.https.onRequest(app);
