46a11a66a7c809c3319555e5b6c2f856dbd41cfb
Savath <savath.khem@live.com>
2019-09-16 23:49:44 -0400

    Middleware 1:30

--- functions/index.js
+++ functions/index.js
@@ -46,8 +46,37 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));   
 })
 
-// Create A Scream
-app.post('/scream', (req, res) => {
+// middleware
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error ('No token found')
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodeToken => {
+        req.user = decodedToken;
+        console.log(docodedToken);
+        return db.collection('user')
+            .where('userId', "==", req.user.uid)
+            .limit(1)
+            .get();
+    })
+    .then(data => {
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch(err => {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json(err);
+    })
+}
+
+// Post one Scream with middleware
+app.post('/scream', FBAuth, (req, res) => {
     if(req.method !== 'POST'){
         return res.status(400).json({ error:'Method not allowed'});
     };
