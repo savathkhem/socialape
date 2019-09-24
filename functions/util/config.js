37bbd3d7f9f7d0dd562b18e6d3b0db63fd95276d
Savath <savath.khem@live.com>
2019-09-23 22:45:06 -0400

    refactor

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyA_Qrkz88sGvPc7MKJTrNSp-wEhgqJALfo",
+    authDomain: "socialape-7585c.firebaseapp.com",
+    databaseURL: "https://socialape-7585c.firebaseio.com",
+    projectId: "socialape-7585c",
+    storageBucket: "socialape-7585c.appspot.com",
+    messagingSenderId: "612462928108",
+    appId: "1:612462928108:web:be75ab3a1957f7a81445c9"
+};
